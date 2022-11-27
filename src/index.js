825250be9d991161f75b0b84f02f7d739c826de8
Badescu Petre Cosmin <badescupetrecosmin@192-168-0-104.rdsnet.ro>
2022-11-27 23:46:50 +0200

    add apollo client; add apollo provider in the root folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+const client = new ApolloClient({
+  uri: "https://flyby-gateway.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+    ,
   </React.StrictMode>
 );
