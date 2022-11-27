1d8981c4e2cbc4e095ff894493976d8cfb3e5d35
Badescu Petre Cosmin <badescupetrecosmin@gmail.com>
2022-11-27 23:56:43 +0200

    add .env file to have a dynamic links based on env; move graphQl configuration to separate file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from "react";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-const client = new ApolloClient({
-  uri: "https://flyby-gateway.herokuapp.com/",
-  cache: new InMemoryCache(),
-});
+import { client } from "./config";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
