984e8eb3e91d213e819af848cea23c3264d1f865
Badescu Petre Cosmin <badescupetrecosmin@gmail.com>
2022-11-28 02:12:35 +0200

    remove unused data from query

--- src/queries/posts-queries.js
+++ src/queries/posts-queries.js
@@ -4,16 +4,7 @@ export const GET_POSTS = gql`
   {
     allPosts(count: 100) {
       id
-      title
-      body
-      published
       createdAt
-      author {
-        id
-        firstName
-        lastName
-        avatar
-      }
     }
   }
 `;
